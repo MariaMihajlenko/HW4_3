007e6b713d0735c752e2ad99bebd6b7e3570e431
MariaMihajlenko <Mariamihajlenko3117@gmail.com>
2022-08-21 15:34:53 +0500

    zadacha4_3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+using System;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите  число:  ");
+        int numbers = Convert.ToInt32(Console.ReadLine());
+        char[] arr = numbers.ToString().ToCharArray();
+        int i = 0;
+        int n = arr.Length;
+        Console.Write("[");
+        for (i = 0; i < n; i++)
+        {
+
+            Console.Write(arr[i] + ",");
+
+        }
+        Console.Write("]");
+    }
+}
